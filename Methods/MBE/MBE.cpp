f173ccdf212ebad71097d64524465d79985806a0
Ryan Richard <ryanmrichard1@gmail.com>
2016-03-18 18:50:42 -0400

    Pretty result printing, still needs tweaking to work with output object

--- Methods/MBE/MBE.cpp
+++ Methods/MBE/MBE.cpp
@@ -10,6 +10,7 @@
 #include <bpmodule/parallel/InitFinalize.hpp>
 #include <bpmodule/math/PowerSetItr.hpp>
 #include <bpmodule/math/Binomial.hpp>
+#include <bpmodule/output/Table.hpp>
 #include <LibTaskForce.hpp>
 #include "Methods/MBE/MBE.hpp"
 
@@ -43,36 +44,34 @@ namespace bpmethods{
              std::string Key,
              unsigned long ID,
              const System& Sys):
-            MM_(MM),Key_(Key),ID_(ID),Sys_(Sys){ }
+                MM_(MM),Key_(Key),ID_(ID),Sys_(Sys){ }
         Return_t operator()(size_t DerivOrder)const{
             EMethod_t Method=MM_.GetModule<EnergyMethod>(Key_,ID_);
             Method->Wfn().system.Set(Sys_);
             return Method->Deriv(DerivOrder);
         }
     };
-    
+
     //Computes n-body interaction by recursion
     Return_t NBodyEgy(size_t DoF,
                       const std::vector<DerivMap>& Ints,
                       const std::vector<DerivMap>& Derivs,
                       const String_t& Frags);
-    
-String_t split(const std::string &s) {
-    String_t elems;
-    std::stringstream ss(s);
-    std::string item;
-    while (std::getline(ss, item, '_'))elems.push_back(item);
-    return elems;
-}
-    
+    String_t split(const std::string &s){
+        String_t elems;
+        std::stringstream ss(s);
+        std::string item;
+        while(std::getline(ss,item,'_'))elems.push_back(item);
+        return elems;
+    }
     Return_t MBE::DerivImpl(size_t Order)const{
         //Load options
         const OptionMap& DaOptions=Options();
         std::string MethodName=DaOptions.Get<std::string>("METHOD");
         size_t N=DaOptions.Get<size_t>("TRUNCATION_ORDER");
         std::map<size_t,double> Truncs=
-            DaOptions.Get<std::map<size_t,double>>("DISTANCE_THRESHOLDS");
-                
+                DaOptions.Get<std::map<size_t,double>>("DISTANCE_THRESHOLDS");
+
         //Compute size of super system basis
         const System& Mol=*Wfn().system;
         size_t DoF=1;
@@ -87,8 +86,8 @@ String_t split(const std::string &s) {
             //Defaults to about 1e308 a.u., or about 1e271 times the size of
             //the observable universe...I think that is equivalent to no cut-off
             double dist=(Truncs.count(n)==1?
-                Truncs.at(n):std::numeric_limits<double>::max());
-            NMers[n-1]=bpmodule::modulebase::MakeNMers(NMers[0],n,dist);  
+                         Truncs.at(n):std::numeric_limits<double>::max());
+            NMers[n-1]=bpmodule::system::MakeNMers(NMers[0],n,dist);
         }
 
         //Run N-Mers
@@ -96,9 +95,9 @@ String_t split(const std::string &s) {
         Communicator NewComm=Comm.Split(Comm.NThreads(),1);
         TaskResults<Return_t> Results(NewComm);
         for(size_t n=N;n>0;--n){
-            for(const auto& NMer : NMers[n-1]){
+            for(const auto& NMer:NMers[n-1]){
                 Results.push_back(NewComm.AddTask(
-                    Task(MManager(),MethodName,ID(),NMer.second),Order));
+                                                  Task(MManager(),MethodName,ID(),NMer.second),Order));
             }
         }
 
@@ -120,15 +119,15 @@ String_t split(const std::string &s) {
                 for(size_t i=0;i<DoF;++i)
                     TotalEgysByOrder[n][i]+=Derivs[n][NMer.first][i];
                 Interactions[n].emplace(NMer.first,
-                    NBodyEgy(DoF,Interactions,Derivs,split(NMer.first))
-                );
+                                        NBodyEgy(DoF,Interactions,Derivs,split(NMer.first))
+                                        );
             }
         }
-        
+
         //Total by summing up all interactions
         std::vector<Return_t> Egy(N,Return_t(DoF,0.0));
         std::vector<Return_t> Egy2(N,Return_t(DoF,0.0));
-        
+
         /* We total two ways:
          *  
          * 1. Sum of all interactions
@@ -146,57 +145,79 @@ String_t split(const std::string &s) {
         const size_t NFrags=NMers[0].size();
         for(size_t BigOrder=1;BigOrder<=N;++BigOrder){
             if(BigOrder>1)Egy2[BigOrder-1]=Egy2[BigOrder-2];
-            for(const auto& Int : Interactions[BigOrder-1])
+            for(const auto& Int:Interactions[BigOrder-1])
                 for(size_t i=0;i<DoF;++i)
                     Egy2[BigOrder-1][i]+=Int.second[i];
             if(BigOrder<N){
                 for(size_t n=0;n<BigOrder;++n){
                     double coef=pow(-1,BigOrder-n-1)*
-                    bpmodule::math::BinomialCoefficient(NFrags-n-2,BigOrder-n-1);
+                            bpmodule::math::BinomialCoefficient(NFrags-n-2,BigOrder-n-1);
                     for(size_t i=0;i<DoF;++i)
-                        Egy[BigOrder-1][i]+=coef*TotalEgysByOrder[n][i];                
+                        Egy[BigOrder-1][i]+=coef*TotalEgysByOrder[n][i];
                 }
-            }
-            else Egy[BigOrder-1]=TotalEgysByOrder[BigOrder-1];
+            }else Egy[BigOrder-1]=TotalEgysByOrder[BigOrder-1];
         }
         /*for(size_t n=0;n<N;n++){
             for(size_t i=0;i<DoF;++i)
                 std::cout<<Egy[n][i]<<" "<<Egy2[n][i]<<std::endl;
-        }*/        
+        }*/
+        bpmodule::output::Table TableOfResults(N+1,3);
+        std::array<std::string,3> ColTitles={"","Closed-Form","SumOfInts"};
+        std::vector<std::string> RowTitles(N);
+        Return_t Col1,Col2;
+        for(size_t i=0;i<N;++i){
+            std::stringstream ss;
+            ss<<i+1<<"-body energy(a.u.):";
+            RowTitles[i]=ss.str();
+            Col1.push_back(Egy[i][0]);
+            Col2.push_back(Egy2[i][0]);
+        }
+        
+        TableOfResults.FillRow(ColTitles,0,0,3);
+        TableOfResults.FillCol(RowTitles,0,1,N+1);
+        TableOfResults.FillCol(Col1,1,1,N+1);
+        TableOfResults.FillCol(Col2,2,1,N+1);
+        TableOfResults.SetHBorder(0,'*');
+        TableOfResults.SetHBorder(1,'-');
+        TableOfResults.SetHBorder(N+1,'*');
+        TableOfResults.SetVBorder(0,'|');
+        TableOfResults.SetVBorder(1,'|');
+        TableOfResults.SetVBorder(3,'|');
+        TableOfResults.SetAlign(0,bpmodule::output::LEFT);
+        std::cout<<TableOfResults<<std::endl;
         return Egy[N-1];
     }
+    /* This function computes the n-body interaction of a given n-mer, recursively.
+     * Say we want the four-body interaction among IJKL, that's the energy
+     * of IJKL less the interactions: IJK, IJL, JKL, IJ, IK, IL, JK, JL, KL, I, J,
+     * K, L, which is the power set of IJKL w/o the empty set
+     */
+    Return_t NBodyEgy(size_t DoF,
+                      const std::vector<DerivMap>& Ints,
+                      const std::vector<DerivMap>& Derivs,
+                      const String_t& Frags){
+        std::stringstream ss;
+        ss<<Frags[0];
+        const size_t n=Frags.size();
+        for(size_t i=1;i<n;++i)ss<<"_"<<Frags[i];
 
-/* This function computes the n-body interaction of a given n-mer, recursively.
- * Say we want the four-body interaction among IJKL, that's the energy
- * of IJKL less the interactions: IJK, IJL, JKL, IJ, IK, IL, JK, JL, KL, I, J,
- * K, L, which is the power set of IJKL w/o the empty set
- */   
-Return_t NBodyEgy(size_t DoF,
-                const std::vector<DerivMap>& Ints,
-                const std::vector<DerivMap>& Derivs,
-                const String_t& Frags){
-    std::stringstream ss;
-    ss<<Frags[0];
-    const size_t n=Frags.size();
-    for(size_t i=1;i<n;++i)ss<<"_"<<Frags[i];
-    
-    //If we already computed this interaction just return
-    if(Ints[n-1].count(ss.str())==1)return Ints[n-1].at(ss.str());
-    
-    //Didn't compute the energy? Assume the interaction is negligible
-    if(Derivs[n-1].count(ss.str())==0)return Return_t();
-    
-    //Otherwise, we're computing it
-    Return_t Egy=Derivs[n-1].at(ss.str());
-    
-    bpmodule::math::PowerSetItr<String_t> SubInts(Frags,1,n-1);
-    while(!SubInts.Done()){
-        Return_t TempEgy=NBodyEgy(DoF,Ints,Derivs,*SubInts);
-        if(TempEgy.size()>0)//Don't subtract out 0 interactions
-            for(size_t i=0;i<DoF;++i)Egy[i]-=TempEgy[i];
-        ++SubInts;
+        //If we already computed this interaction just return
+        if(Ints[n-1].count(ss.str())==1)return Ints[n-1].at(ss.str());
+
+        //Didn't compute the energy? Assume the interaction is negligible
+        if(Derivs[n-1].count(ss.str())==0)return Return_t();
+
+        //Otherwise, we're computing it
+        Return_t Egy=Derivs[n-1].at(ss.str());
+
+        bpmodule::math::PowerSetItr<String_t> SubInts(Frags,1,n-1);
+        while(!SubInts.Done()){
+            Return_t TempEgy=NBodyEgy(DoF,Ints,Derivs,*SubInts);
+            if(TempEgy.size()>0)//Don't subtract out 0 interactions
+                for(size_t i=0;i<DoF;++i)Egy[i]-=TempEgy[i];
+            ++SubInts;
+        }
+        return Egy;
     }
-    return Egy;
-}
 
 }//End namespace
