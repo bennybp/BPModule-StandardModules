9442dd59ff52067ba3588c695bb22a70f4489654
Ryan Richard <ryanmrichard1@gmail.com>
2016-04-08 12:31:41 -0400

    missed file

--- Methods/CBS/Feller.py
+++ Methods/CBS/Feller.py
@@ -1,7 +1,20 @@
 import bpmodule as bp
 
 class FellerCBS(bp.modulebase.EnergyMethod):
-  """This module implements the three-point Helgaker
+  """This module implements the three-point Feller extrapolation
+
+  In the Feller extrapolation scheme, the energy for a basis set of cardinal 
+  number \f$X\f$ is given by:
+  \f[
+    E_X=E_{CBS}+\alpha\exp(-\beta X)
+  \f]
+  For three basis sets of cardinal numbers: \f$X\f$,\f$X+1\f$, and \f$X+2\f$,
+  the equations can be rearranged to give:
+  \f[
+     E_{CBS}=E_X-\frac{\left(E_X-E_{X+1}\right)^2}{E_X-2E_{X+1}-E_{X+2}}
+  \f]
+
+  """
   def __init__(self, myid):
     super(HelgakerCBS, self).__init__(myid)
 
