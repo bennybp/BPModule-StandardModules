0e4dc257e885649b9352d229bfc34e2e1f78a4c7
ryanmrichard <ryanmrichard1@gmail.com>
2017-01-20 12:52:05 -0500

    move to new new build system

--- pulsar_modules/methods/mbe/MBEHelper.py
+++ pulsar_modules/methods/mbe/MBEHelper.py
@@ -1,6 +1,31 @@
 import pulsar as psr
 
 def MBE_wrapper(mm,wfn,truncation_order,MBEMod,nmerizer_key,sub_keys=[]):
+    """ Relies on Pulsar's cacheing ability to compute 1-body, 2-body, etc.
+        derivatives.
+
+        EnergyMethod modules return the best energy they computed.  In the case
+        of the MBE this is the n-body energy; however, all the information for
+        computing the (n-1)-body, the (n-2)-body, etc. energies is available.
+        Within the Pulsar framework the way to get at these energies is to
+        simply loop over n requesting the energy (this is effecient because of
+        Pulsar's cacheing system).  This function is a wrapper that does just
+        that.  Additionally, this function will also, optionally, loop over
+        EnergyMethod modules.  This is intended to be used when the target
+        computation was something like CCSD(T).   In this case, we know that the
+        information required to compute the HF, MP2, and CCSD energies, via the
+        MBE, are is available.  Again, within Pulsar, the way to do this is to
+        loop over the methods and rely on the cacheing system to do this
+        effeciently.
+
+        Args:
+        mm: the ModuleManager instance to use
+        wfn: the Wavefunction instance to use
+        truncation_order: Consider unions of up to how many monomers?
+        MBEmod: the MBE EnergyMethod module instance to call
+        nmerizer_key: the key for Fragmentizer that makes the n-mers
+        sub_keys: a list of EnergyMethod keys to call
+    """
     Derivs={meth:{} for meth in sub_keys}
     for i in range(truncation_order,0,-1):
         newkey=nmerizer_key+str(i)
