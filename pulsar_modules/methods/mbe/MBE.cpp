62bdc42b1ec74af0e9ffd58b4b22810f96ed2099
ryanmrichard <ryanmrichard1@gmail.com>
2017-01-23 17:24:13 -0500

    More-like core build system

--- pulsar_modules/methods/mbe/MBE.cpp
+++ pulsar_modules/methods/mbe/MBE.cpp
@@ -1,13 +1,9 @@
 
 #include<pulsar/modulebase/SystemFragmenter.hpp>
 #include<pulsar/util/IterTools.hpp>
+#include<pulsar/math/Cast.hpp>
 #include "pulsar_modules/methods/mbe/MBE.hpp"
 #include "pulsar_modules/methods/method_helpers/MethodHelpers.hpp"
-
-/*
- * TODO once computations cache results, compute interactions by
- *   "Re-running" the computations and then assembling derivatives
- */
 
 //Options
 const std::string method_key="METHOD_KEY";
@@ -20,7 +16,7 @@ DerivReturnType MBE::deriv_(size_t Order,const Wavefunction& Wfn){
     const System& Mol=*Wfn.system;
     NMerSetType NMers=
             create_child_from_option<SystemFragmenter>(system_key)->fragmentize(Mol);
-    std::vector<double> Cs,Result(std::pow(3*Mol.size(),Order));
+    std::vector<double> Cs,Result(numeric_cast<size_t>(std::pow(3*Mol.size(),Order)));
     std::vector<Wavefunction> Wfns;
     for(const typename NMerSetType::value_type& NMerI : NMers){
         Cs.push_back(NMerI.second.weight);
--- test/analysis/TestMBEHelper.py
+++ test/analysis/TestMBEHelper.py
@@ -1,8 +1,9 @@
 import os
 import sys
 import pulsar as psr
-from pulsar_modules.analysis.MBEHelper import MBE_wrapper
 sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+print(sys.path)
+from pulsar_modules.analysis.MBEHelper import MBE_wrapper
 
 def run(mm):
     tester=psr.PyTester("Testing the MBEHelper Analysis Module")
